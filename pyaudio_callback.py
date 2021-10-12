d0b759da611add17725765589dc79d04f12c2d4d
Ben Perche <benperche2@gmail.com>
2021-10-12 14:52:58 +1100

    Tidying

--- pyaudio_callback.py
+++ pyaudio_callback.py
@@ -14,8 +14,8 @@ import multiprocessing as mp
 import time
 
 import config
-import audiodevices
-import roslistener
+import audio_devices
+import ros_interface
 from tone import Tone, ClassTones
 
 # ROS
@@ -38,13 +38,10 @@ outbuf = np.zeros(config.FRAMES_PER_BUFFER*config.CHANNELS,dtype=np.int16)
 # Callback function which is called by pyaudio
 #   whenever it needs output-data or has input-data
 def audioCallback(in_data, frame_count, time_info, status):
-    # global tone.ClassTones
     global AUDIOTIME
     global ClassTones
     global sound_array
     global outbuf
-    # global audio_pipe
-    # global ros_pipe
 
     # Reset arrays
     sound_array.fill(0)
@@ -62,13 +59,6 @@ def audioCallback(in_data, frame_count, time_info, status):
         print('Callback status ', status)
 
 
-    # Check if there is new data to be read
-    # print(ClassTones[0].fundFreq)
-    # if audio_pipe.poll():
-    #     ClassTones = audio_pipe.recv()
-    #     print('Data recv')
-    # print(ClassTones[0].fundFreq)
-
     # Loop through the number of Tones to compute their relevant contribution
     for currentTone in ClassTones:
 
@@ -80,7 +70,6 @@ def audioCallback(in_data, frame_count, time_info, status):
         # Update phase of this tone to compute the next value
         currentTone.updatePhaseArray()
 
-    # print(sound_array)
     # Copy the generated sounds into the output buffer array, which stores the
     # values for each channel interleaved LRLRLRLR etc
 
@@ -101,73 +90,41 @@ def audioCallback(in_data, frame_count, time_info, status):
     print('Time to execute callback ', since_start)
     AUDIOTIME = time.time()
 
-    # print('Fund freq postcall', Tone.fundFreq)
-
     return (out, pyaudio.paContinue)
+
+
 
 #########################
 # Start of main program #
 #########################
 
-
-
-def ros_setup(shared_tones):
-    # global ros_pipe
-
-
-    print('ROS Setup Fn')
-    rospy.init_node('audio_listener')
-    audio_autonomous = roslistener.AudioAutonomous(shared_tones)
-
-    roscount=0
-    while not rospy.is_shutdown():
-        roscount += 1
-        if roscount > 500000:
-            roscount = 0
-
-            print('Spinning ros thread')
-
-
 def main():
-    # global tone.ClassTones
-    # global audio_pipe
-
-    # Make a pipe to transfer the Tone objects
-    # global audio_pipe
-    # global ros_pipe
-    # ros_pipe, audio_pipe = mp.Pipe()
-    # global shared_tones
 
     # Create single element queue to pass the frequency between the ROS reader
     # process and this process
     shared_fundFreq = mp.Queue(1)
     shared_fundFreq.put(config.INITIAL_FREQUENCY)
 
-
-    # Start ros listener in a separate process/thread
-    # mp.set_start_method('spawn')
-
     # Start a separate process to read ROS velocity messages
-    r = mp.Process(target = ros_setup, args=[shared_fundFreq])
+    r = mp.Process(target = ros_interface.ros_start, args=[shared_fundFreq])
     r.start()
 
-    # Update all tones
+    # Update all tones with initial frequency
     for currentTone in ClassTones:
             currentTone.updateFrequency()
-
 
     # get a handle to the pyaudio interface
     paHandle = pyaudio.PyAudio()
 
-    audiodevices.showDevices(paHandle)
+    audio_devices.showDevices(paHandle)
 
     # select a device
-    audiodevices.outputDevice = audiodevices.setDefaultOutputDevice(paHandle)
-    devinfo = paHandle.get_device_info_by_index(audiodevices.outputDevice)
+    audio_devices.outputDevice = audio_devices.setDefaultOutputDevice(paHandle)
+    devinfo = paHandle.get_device_info_by_index(audio_devices.outputDevice)
     print("Selected device name: ", devinfo.get('name'))
 
     support = paHandle.is_format_supported(rate=config.RATE,input_device=None,
-                input_format=None,output_device=audiodevices.outputDevice, output_channels=2,
+                input_format=None,output_device=audio_devices.outputDevice, output_channels=2,
                 output_format=paHandle.get_format_from_width(config.WIDTH))
 
     print('Is format supported: ', support)
@@ -179,38 +136,36 @@ def main():
                            frames_per_buffer=config.FRAMES_PER_BUFFER,
                            input=False,  # no input
                            output=True,  # only output
-                           output_device_index=audiodevices.outputDevice,
+                           output_device_index=audio_devices.outputDevice,
                            stream_callback=audioCallback)
 
     stream.start_stream()
     print('Stream Started')
 
-    # Setup frequency sweep values to be used in loop
+    # Setup count to periodically print stats during main loop
     count = 0
 
 
-    # Loop while new info comes in
+    # Main Loop
     while not rospy.is_shutdown(): #stream.is_active():
 
         # Periodically check for new speed values from ROS and update the phase arrays
         if shared_fundFreq.full():
+            print('Received New Freq')
             # Update the fundamental frequency for all the tones
             Tone.fundFreq=shared_fundFreq.get()
 
             # Fix all the other frequencies
             for currentTone in ClassTones:
                     currentTone.updateFrequency()
+            print('Updated')
 
-        # pass
-        # When we receive a new velocity, change the frequeny of the output sound
-        # For now, gradually change direction of sine wave
+        # Every so often print CPU Load
         count += 1
         if count > 500000:
             count = 0
 
             print('CPU Load ', stream.get_cpu_load())
-
-
 
     stream.stop_stream()
     stream.close()
--- ros_interface.py
+++ ros_interface.py
@@ -9,6 +9,24 @@ import multiprocessing as mp
 import config
 from tone import Tone, ClassTones
 
+# Function to set up listener object to vehicle velocity
+# This can be called in a separate process to improve performance
+def ros_start(shared_tones):
+
+    rospy.init_node('audio_listener')
+    audio_autonomous = AudioAutonomous(shared_tones)
+
+    print('ROS Setup Completed')
+
+    # Do nothing in main loop
+    roscount=0
+    while not rospy.is_shutdown():
+        roscount += 1
+        if roscount > 500000:
+            roscount = 0
+            print('Spinning ros thread')
+
+
 class AudioAutonomous:
 
     # Setup listeners
@@ -17,55 +35,26 @@ class AudioAutonomous:
 
         self.current_velocity = 0.
 
+        # Method for passing message back to main process
         self.shared_fundFreq = shared_fundFreq
 
-        # self.ROSTIME = datetime.datetime.now()
-
         rospy.Subscriber('/zio/odometry/rear', Odometry, self.odo_reader)
-
-        # # Setup frequency sweep values to be used in loop
-        # self.count = 0
-        # self.ascending = True
-        #
-        # # Temporary sweep parameters
-        # self.UPPER_FREQ = 270
-        # self.LOWER_FREQ = 175
-        # self.STEP = 2#0.25
-
-        # self.output_pipe = ros_pipe
-
-
 
     # Callback to store velocity info
     def odo_reader(self, data):
 
-        # global ClassTones
-
+        # Read new velocity
         self.current_velocity = data.twist.twist.linear.x
         # print('NewVel')
 
-        # Get any new values from the pipe
-        # print(ClassTones[0].fundFreq)
-        # if self.output_pipe.poll():
-        #     print('Polling')
-        #     ClassTones = self.output_pipe.recv()
-        # print(ClassTones[0].fundFreq)
-
-        # Tone.fundFreq = self.current_velocity * 100
+        # Calculate frequency to be returned
+        newFreq = 25 * self.current_velocity + 120
 
         # Empty queue if needed
         if self.shared_fundFreq.full():
             self.shared_fundFreq.get()
 
         # Put new value into the queue, overwriting previous value
-        newFreq = 25 * self.current_velocity + 120
         self.shared_fundFreq.put(newFreq, False)
 
-        # for currentTone in self.ros_class_tones[0]:
-        #         currentTone.updateFrequency()
-
         print('Velocity = ', self.current_velocity, ' frequency = ',newFreq)
-
-
-        # self.output_pipe.send(ClassTones)
-        # print('Output sent')
